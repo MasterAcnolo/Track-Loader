0b23b2da1998e4f7ea49d5b19546d94ee550c7d9
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-25 16:27:50 +0100

    Add: Test .env feat

---
+++ api/.env.php
@@ -0,0 +1,7 @@
+<?php return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => '',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'root',
+];
+?>
---
+++ api/config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require '../.env.php';
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']};charset=utf8",
+        $config['DB_USER'],
+        $config['DB_PASS']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur DB : " . $e->getMessage());
+};
+
+echo $config['DB_USER'];
