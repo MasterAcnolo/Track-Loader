4c29ce998cfd71e7d70f2bd4da3a205b76be4f52
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-26 14:43:09 +0100

    Feat: /status pages

---
+++ api/index.php
@@ -0,0 +1,22 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+$fullPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$cleanPath = trim(str_replace('/Track-Loader/api', '', $fullPath), '/');
+$path = '/' . $cleanPath;
+
+switch (true) {
+    case $path === "/status":
+        require __DIR__ . '/routes/StatusRoutes.php';
+        exit;
+
+    case str_starts_with($path, "/user"):
+        require __DIR__ . '/routes/UserRoutes.php';
+        exit;
+}
+
+http_response_code(404);
+echo json_encode(["error" => "Route not found"]);
+
+?>
