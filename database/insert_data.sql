91b2297e95c43d6e3702a2686bdd78219487628d
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-27 16:05:03 +0100

    Fix: Data Style to avoid mismatch

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -3,14 +3,14 @@ INSERT INTO `Album`
 VALUES
 
 -- ============================================================
---  LINKIN PARK  |  Rock Alternatif
+--  LINKIN PARK  |  Rock-Alternatif
 -- ============================================================
 
 -- 1. Hybrid Theory (2000) — MBID release : 5fc6445c-05ce-34e7-ab31-63bf7d3a9f24
 (
   'Hybrid Theory',
   'https://m.media-amazon.com/images/I/71PlJet28jL._AC_UF1000,1000_QL80_.jpg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Papercut","One Step Closer","With You","Points of Authority","Crawling","Runaway","By Myself","In the End","A Place for My Head","Forgotten","Cure for the Itch","Pushing Me Away", "My December", "High Voltage"]',
   '2000-10-24',
   12,
@@ -22,7 +22,7 @@ VALUES
 (
   'Meteora',
   'https://m.media-amazon.com/images/I/81SnDmLBHQL.jpg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Foreword","Don''t Stay","Somewhere I Belong","Lying from You","Hit the Floor","Easier to Run","Faint","Figure.09","Breaking the Habit","From the Inside","Nobody''s Listening","Session","Numb"]',
   '2003-03-25',
   12,
@@ -34,7 +34,7 @@ VALUES
 (
   'Minutes to Midnight',
   'https://i.redd.it/e1r808iuuypd1.jpeg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Wake","Given Up","Leave Out All the Rest","Bleed It Out","Shadow of the Day","What I''ve Done","Hands Held High","No More Sorrow","Valentine''s Day","In Between","In Pieces","The Little Things Give You Away"]',
   '2007-05-14',
   13,
@@ -46,7 +46,7 @@ VALUES
 (
   'A Thousand Suns',
   'https://m.media-amazon.com/images/I/81Z8E8G1OEL.jpg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["The Requiem","The Radiance","Burning in the Skies","Empty Spaces","When They Come for Me","Robot Boy","Jornada del Muerto","Waiting for the End","Blackout","Wretches and Kings","Wisdom, Justice, and Love","Iridescent","Fallout","The Catalyst","The Messenger"]',
   '2010-09-14',
   13,
@@ -58,7 +58,7 @@ VALUES
 (
   'Living Things',
   'https://i.redd.it/2ru16onv7pff1.jpeg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Lost in the Echo","In My Remains","Burn It Down","Lies Greed Misery","I''ll Be Gone","Castle of Glass","Victimized","Roads Untraveled","Skin to Bone","Until It Breaks","Tinfoil","Powerless"]',
   '2012-06-26',
   13,
@@ -70,7 +70,7 @@ VALUES
 (
   'The Hunting Party',
   'https://linkinpedia.com/w/images/thumb/1/13/Studio-The_Hunting_Party_cover.jpg/1200px-Studio-The_Hunting_Party_cover.jpg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Keys to the Kingdom","All for Nothing (feat. Page Hamilton)","Guilty All the Same (feat. Rakim)","The Summoning","War","Wastelands","Until It''s Gone","Rebellion (feat. Daron Malakian)","Mark the Graves","Drawbar (feat. Tom Morello)","Final Masquerade","A Line in the Sand"]',
   '2014-06-13',
   13,
@@ -82,7 +82,7 @@ VALUES
 (
   'One More Light',
   'https://i.redd.it/k4cmj7cexqqf1.jpeg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["Nobody Can Save Me","Good Goodbye (feat. Pusha T & Stormzy)","Talking to Myself","Battle Symphony","Invisible","Heavy (feat. Kiiara)","Sorry for Now","Halfway Right","One More Light","Sharp Edges"]',
   '2017-05-19',
   13,
@@ -94,7 +94,7 @@ VALUES
 (
   'From Zero',
   'https://groundzero.fr/wp-content/uploads/2024/09/Linkin_Park_-_From_Zero.jpeg',
-  'Rock Alternatif',
+  'Rock-Alternatif',
   '["From Zero (Intro)","The Emptiness Machine","Cut the Bridge","Heavy Is the Crown","Over Each Other","Casualty","Overflow","Two Faced","Stained","IGYEIH","Good Things Go"]',
   '2024-11-15',
   14,
@@ -103,14 +103,14 @@ VALUES
 ),
 
 -- ============================================================
---  NF  |  Hip Hop
+--  NF  |  Hip-Hop
 -- ============================================================
 
 -- 9. Mansion (2015) — MBID release : 8db1bc0d-2d76-428b-8c0a-b4e71c98a033
 (
   'Mansion',
   'https://m.media-amazon.com/images/I/71o6fTQjpcL._UF894,1000_QL80_.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Intro", "Mansion", "All I Have", "Wait", "Wake Up", "Face It", "Motivated", "Notepad", "Turn The Music Up", "Paralyzed", "I''l Keep On, "Can You Hold Me"]',
   '2015-03-31',
   10,
@@ -122,7 +122,7 @@ VALUES
 (
   'Therapy Session',
   'https://m.media-amazon.com/images/I/91DAMdLVM2L._UF1000,1000_QL80_.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Intro II", "Therapy Session", "I Just Wanna Know", "How Could You Leave Us", "Breathe", "Real", "Oh Lord", "I Can Fell It", "Got You On My Mind", "Grindin''", "Wish You Woudldn''t, "Statement", "All I Do", "Lost In The Moment"]',
   '2016-04-22',
   10,
@@ -134,7 +134,7 @@ VALUES
 (
   'Perception',
   'https://m.media-amazon.com/images/I/713VZ3I1O-L._UF894,1000_QL80_.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Intro III", "Outcast", "10 Feet Down", "Green Lights", "Dreams", "Let You Down", "Destiny", "My Life", "You''re Special", "If You Want Love, "Remember This", "Know", "Lie", "3 A.M.", "One Hundred", "Outro"]',
   '2017-10-06',
   11,
@@ -146,7 +146,7 @@ VALUES
 (
   'The Search',
   'https://media.pitchfork.com/photos/5d4864ac04c06a00092f65da/master/pass/NF_TheSearch.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["The Search","Leave Me Alone","Change", "My Stress", "Nate", "Time", "Returns", "When I Grow Up", "Only", "Let Me Go", "-Interlude-", "Hate Myself", "I Miss The Days", "No Excuses", "Like This", "Options", "WHY", "Trauma", "Time-Edit"]',
   '2019-07-26',
   12,
@@ -158,7 +158,7 @@ VALUES
 (
   'CLOUDS (THE MIXTAPE)',
   'https://m.media-amazon.com/images/I/610fVAc+f2L.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["CLOUDS","THAT''S A JOKE","JUST LIKE YOU","STORY","PRIDEFUL","LOST (feat. Hopsin)","LAYERS","DRIFTING","TRUST (feat. Tech N9ne)","PAID MY DUES", "CLOUDS-Edit"]',
   '2021-03-26',
   11,
@@ -170,7 +170,7 @@ VALUES
 (
   'HOPE',
   'https://static.fnac-static.com/multimedia/Images/FR/NR/db/82/e7/15172315/1540-1/tsp20230620161439/Hope.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["HOPE","MOTTO", "CAREFUL", "MAMA", "HAPPY", "PANDEMONIUM", "SUFFICE", "GONE", "BULLET", "TURN MY BACK", "MISTAKE", "LET EM PRAY", "RUNNING"]',
   '2023-04-07',
   12,
@@ -182,7 +182,7 @@ VALUES
 (
   'FEAR',
   'https://cdn-images.dzcdn.net/images/cover/235dda4eca7980c712553e847d49459a/500x500.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["FEAR","HOME","WHO I WAS (feat. mgk)","GIVE ME A REASON","SORRY (feat. James Arthur)","WASHED UP"]',
   '2025-11-14',
   10,
@@ -191,14 +191,14 @@ VALUES
 ),
 
 -- ============================================================
---  EMINEM  |  Hip Hop
+--  EMINEM  |  Hip-Hop
 -- ============================================================
 
 -- 16. The Slim Shady LP (1999) — MBID release : fd82c0c8-1b4e-3e4a-a8c2-1e3f5c7d9e0a
 (
   'The Slim Shady LP',
   'https://m.media-amazon.com/images/I/81Ahe2x9qBL.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Public Service Announcement","My Name Is","Guilty Conscience (feat. Dr. Dre)","Brain Damage","Paul (Skit)","If I Had...","97 Bonnie & Clyde","Bitch","Role Model","Lounge (Skit)","My Fault","Ken Kaniff (Skit)","Cum on Everybody","Rock Bottom","Just Don''t Give a F***","Soap (Skit)","As the World Turns","I''m Shady","Bad Meets Evil","Still Don''t Give a F***"]',
   '1999-02-23',
   11,
@@ -210,7 +210,7 @@ VALUES
 (
   'The Marshall Mathers LP',
   'https://static.fnac-static.com/multimedia/Images/FR/NR/6b/bf/10/1097579/1507-1/tsp20230712131243/The-Marshall-Mathers-LP.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Public Service Announcement 2000","Kill You","Stan","Paul (Skit)","Who Knew","Steve Berman (Skit)","The Way I Am","The Real Slim Shady","Remember Me? (feat. RBX & Sticky Fingaz)","I''m Back","Marshall Mathers","Ken Kaniff (Skit)","Drug Ballad","Amityville (feat. Bizarre)","Bitch Please II (feat. Dr. Dre, Snoop Dogg, Xzibit & Nate Dogg)","Kim","Under the Influence (feat. D12)","Criminal"]',
   '2000-05-23',
   12,
@@ -222,7 +222,7 @@ VALUES
 (
   'The Eminem Show',
   'https://m.media-amazon.com/images/I/71n0xmxpw7L.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Curtains Up (Skit)","White America","Business","Cleanin'' Out My Closet","Square Dance","The Kiss (Skit)","Soldier","Say Goodbye Hollywood","Drips (feat. Obie Trice)","Without Me","Paul Rosenberg (Skit)","Sing for the Moment","Superman (feat. Dina Rae)","Hailie''s Song","Steve Berman (Skit)","When the Music Stops (feat. D12)","Say What You Say (feat. Dr. Dre)","Till I Collapse (feat. Nate Dogg)","My Dad''s Gone Crazy (feat. Hailie Jade)","Curtains Close (Skit)"]',
   '2002-05-26',
   12,
@@ -234,7 +234,7 @@ VALUES
 (
   'Encore',
   'https://m.media-amazon.com/images/I/61iN2uEt2ML.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Curtains Up","Evil Deeds","Never Enough (feat. Nate Dogg & 50 Cent)","Yellow Brick Road","Like Toy Soldiers","Mosh","Puke","My 1st Single","Paul (Skit)","Rain Man","Big Weenie","Em Calls Paul (Skit)","Just Lose It","Ass Like That","Spend Some Time (feat. Obie Trice, Stat Quo & 50 Cent)","Mockingbird","Crazy in Love","One Shot 2 Shot (feat. D12)","Final Thought (Skit)","Encore / Curtains Down (feat. Dr. Dre & 50 Cent)"]',
   '2004-11-12',
   12,
@@ -246,7 +246,7 @@ VALUES
 (
   'Relapse',
   'https://cdn-images.dzcdn.net/images/cover/bd571081cf7d4ee6fe9e5904423b76cb/1900x1900-000000-80-0-0.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Dr. West (Skit)","3 A.M.","My Mom","Insane","Bagpipes from Baghdad","Hello","Tonya (Skit)","Same Song & Dance","We Made You","Medicine Ball","Paul (Skit)","Stay Wide Awake","Old Times Sake (feat. Dr. Dre)","Must Be the Ganja","Mr. Mathers (Skit)","Deja Vu","Beautiful","Crack a Bottle (feat. Dr. Dre & 50 Cent)","Steve Berman (Skit)","Underground","My Darling","Careful What You Wish For"]',
   '2009-05-15',
   12,
@@ -258,7 +258,7 @@ VALUES
 (
   'Recovery',
   'https://cdn-images.dzcdn.net/images/cover/be682506145061814eddee648edb7c59/0x1900-000000-80-0-0.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Cold Wind Blows","Talkin'' 2 Myself (feat. Kobe)","On Fire","Won''t Back Down (feat. P!nk)","W.T.P.","Going Through Changes","Not Afraid","Seduction","No Love (feat. Lil Wayne)","Space Bound","Cinderella Man","25 to Life","So Bad","Almost Famous","Love the Way You Lie (feat. Rihanna)","You''re Never Over","Untitled"]',
   '2010-06-18',
   12,
@@ -270,7 +270,7 @@ VALUES
 (
   'The Marshall Mathers LP 2',
   'https://m.media-amazon.com/images/I/91E0tsoLNrL.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Bad Guy","Parking Lot (Skit)","Rhyme or Reason","So Much Better","Survival","Legacy","Asshole (feat. Skylar Grey)","Berzerk","Rap God","Brainless","Stronger Than I Was","The Monster (feat. Rihanna)","So Far...","Love Game (feat. Kendrick Lamar)","Headlights (feat. Nate Ruess)","Evil Twin"]',
   '2013-11-05',
   13,
@@ -282,7 +282,7 @@ VALUES
 (
   'Revival',
   'https://static.fnac-static.com/multimedia/Images/FR/NR/13/1f/81/8462099/1540-1/tsp20171208102200/Revival.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Walk on Water (feat. Beyoncé)","Believe","Like Home (feat. Alicia Keys)","Bad Husband (feat. X Ambassadors)","Tragic Endings (feat. Skylar Grey)","Framed","Remind Me (Intro)","Remind Me (feat. MNEK)","River (feat. Ed Sheeran)","Nowhere Fast (feat. Kehlani)","Heat (feat. Phresher)","Offended","Need Me (feat. Pink)","In Your Head (feat. Skylar Grey)","Castle","Arose"]',
   '2017-12-15',
   13,
@@ -294,7 +294,7 @@ VALUES
 (
   'Kamikaze',
   'https://m.media-amazon.com/images/I/91AMfe72gCL._UF894,1000_QL80_.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["The Ringer","Greatest","Lucky You (feat. Joyner Lucas)","Paul (Skit)","Normal","Em Calls Paul (Skit)","Stepping Stone","Not Alike (feat. Royce da 5''9\")","Kamikaze","Fall","Nice Guy (feat. Jessie Reyez)","Good Guy (feat. Jessie Reyez)","Venom"]',
   '2018-08-31',
   13,
@@ -306,7 +306,7 @@ VALUES
 (
   'Music to Be Murdered By',
   'https://m.media-amazon.com/images/I/91EMDOSuZVL.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Premonition (Intro)","Unaccommodating (feat. Young M.A)","Those Kinda Nights (feat. Ed Sheeran)","In Too Deep","Godzilla (feat. Juice WRLD)","Darkness","Leaving Heaven (feat. Skylar Grey)","Yah Yah (feat. Royce da 5''9\", Black Thought, Q-Tip & Denaun)","Stepdad (Skit)","Marsh","Never Love Again","Little Engine","Lock It Up (feat. Anderson .Paak)","Farewell","No Regrets (feat. Don Toliver)","I Will (feat. KXNG Crooked, Royce da 5''9\" & Joell Ortiz)"]',
   '2020-01-17',
   14,
@@ -318,7 +318,7 @@ VALUES
 (
   'The Death of Slim Shady (Coup de Grâce)',
   'https://m.media-amazon.com/images/I/81aMOa9KUQL._UF1000,1000_QL80_.jpg',
-  'Hip Hop',
+  'Hip-Hop',
   '["Renaissance","Habits (feat. White Gold)","Lucifer (feat. JID)","Antichrist","Bad One (feat. JID & Skylar Grey)","Brand New Dance","Houdini","Tobey (feat. Big Sean & BabyTron)","Fuel (feat. JID & GRIP)","Guilty Conscience 2","Head Honcho (feat. Denaun & Lefty Gunplay)","Road Rage","Somebody Save Me (feat. Jelly Roll)","Temporary","Dying","Evil","Guess Who''s Back","The Death of Slim Shady"]',
   '2024-07-12',
   14,
