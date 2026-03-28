0e580622c104cc46e7efd7a2b692b9ea2d677f62
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-03-28 17:05:40 +0100

    Fix: Remove unused formatTracklist function from helpers.php

--- api/helpers/helpers.php
+++ api/helpers/helpers.php
@@ -1,18 +1,4 @@
 <?php
-// Helpers pour l'API Albums
-function formatTracklist($album) {
-    if (!empty($album['tracklist'])) {
-        $tracks = json_decode($album['tracklist'], true);
-        if (is_array($tracks)) {
-            $newTracks = [];
-            foreach ($tracks as $index => $track) {
-                $newTracks[$index + 1] = $track;
-            }
-            $album['tracklist'] = $newTracks;
-        }
-    }
-    return $album;
-}
 
 function sendJson($code, $data) {
     http_response_code($code);
